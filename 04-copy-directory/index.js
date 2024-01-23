26a60706243c8a32f711ac7fb804bced84587051
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-01-23 11:07:49 +0300

    fix: fix directory update in 04-copy-folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,23 +5,29 @@ const fsPromises = fs.promises;
 const pathSource = path.join(__dirname, "files");
 const pathCopy = path.join(__dirname, "files-copy");
 
-fsPromises.mkdir(
+fsPromises.rm(
   pathCopy,
-  { recursive: true },
-)
-
-fs.readdir(
-  pathSource,
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) console.log(err.message);
-    files.forEach((file) => {
-      const pathFile = path.join(pathSource, file.name);
-      const pathFileCopy = path.join(pathCopy, file.name);
-      fsPromises.copyFile(
-        pathFile,
-        pathFileCopy
-      )
-    })
-  }
-)
+  { recursive: true, force: true })
+  .then(() => {
+    fsPromises.mkdir(
+      pathCopy,
+      { recursive: true },
+    )
+  })
+  .then(() => {
+    fs.readdir(
+      pathSource,
+      { withFileTypes: true },
+      (err, files) => {
+        if (err) console.log(err.message);
+        files.forEach((file) => {
+          const pathFile = path.join(pathSource, file.name);
+          const pathFileCopy = path.join(pathCopy, file.name);
+          fsPromises.copyFile(
+            pathFile,
+            pathFileCopy
+          )
+        })
+      }
+    )
+  })
